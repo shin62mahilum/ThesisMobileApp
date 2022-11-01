2ce93e5092646c75f9ad9bc6424544ae2e1742b3
Shin <shin62mahilum@gmail.com>
2022-11-01 09:18:40 +0800

    Added 2 Activities (adminLogin and userLogin)

---
+++ app/src/main/java/com/example/thesismobileapp/userLogin.java
@@ -0,0 +1,14 @@
+package com.example.thesismobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class userLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_login);
+    }
+}
