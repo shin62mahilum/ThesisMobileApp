677f003ca766f87aeda7015eebb6f9b82e722c3c
Shin <shin62mahilum@gmail.com>
2022-11-01 17:44:37 +0800

    Added more button functions (Admin and User Interfaces )

--- app/src/main/java/com/example/thesismobileapp/adminLogin.java
+++ app/src/main/java/com/example/thesismobileapp/adminLogin.java
@@ -2,13 +2,31 @@ package com.example.thesismobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class adminLogin extends AppCompatActivity {
+
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login2);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAdminInterface();
+            }
+        });
+    }
+
+    public void  openAdminInterface() {
+        Intent intent = new Intent(this, adminInterface.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/thesismobileapp/userLogin.java
+++ app/src/main/java/com/example/thesismobileapp/userLogin.java
@@ -2,13 +2,31 @@ package com.example.thesismobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class userLogin extends AppCompatActivity {
 
+    private Button btnLogin;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openUserInterface();
+            }
+        });
+    }
+
+    public void  openUserInterface() {
+        Intent intent = new Intent(this, userInterface.class);
+        startActivity(intent);
     }
 }
